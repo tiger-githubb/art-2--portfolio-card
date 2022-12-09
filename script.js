36edc0e0162619bdcff92e8ff1e0cf71f0f5eec1
tiger-githubb <aristidetiger12@gmail.com>
2022-12-09 11:11:52 +0000

    final version

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const gallery = document.getElementById("gallery");
 
-
-window.onmouseover = e => {
-    const mouseX = e.clientX,
-          mouseY = e.clientY;
-
-    const xDecimal = mouseX / window.innerWidth,
-          yDecimal = mouseY / window.innerHeight;
-
-    const panX = gallery.offsetWidth * xDecimal * -1,        
-          panY = gallery.offsetHeight * yDecimal * -1;
-
-    gallery.style.transform = `translate(${panX}px, ${panY}px)`
+window.onmousemove = e => {
+  const mouseX = e.clientX,
+        mouseY = e.clientY;
+  
+  const xDecimal = mouseX / window.innerWidth,
+        yDecimal = mouseY / window.innerHeight;
+  
+  const maxX = gallery.offsetWidth - window.innerWidth,
+        maxY = gallery.offsetHeight - window.innerHeight;
+  
+  const panX = maxX * xDecimal * -1,
+        panY = maxY * yDecimal * -1;
+  
+  gallery.animate({
+    transform: `translate(${panX}px, ${panY}px)`
+  }, {
+    duration: 4000,
+    fill: "forwards",
+    easing: "ease"
+  })
 }
